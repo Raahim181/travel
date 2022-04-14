344496fddb4ad953bff5a1176918cbbffe3ca9cb
haresh408 <78948375+haresh408@users.noreply.github.com>
2022-04-14 17:08:30 +0530

    Add files via upload

---
+++ book_form.php
@@ -0,0 +1,29 @@
+<?php
+    $server="localhost:3307";
+    $username="root";
+    $password="";
+    $db="book_db";
+    
+
+   $connection = mysqli_connect($server,$username,$password,$db);
+
+   if(isset($_POST['send'])){
+      $name = $_POST['name'];
+      $email = $_POST['email'];
+      $phone = $_POST['phone'];
+      $address = $_POST['address'];
+      $location = $_POST['location'];
+      $guests = $_POST['guests'];
+      $arrival = $_POST['arrival'];
+      $leaving = $_POST['leaving'];
+
+      $request = " insert into book_form(name, email, phone, address, location, guests, arrival, leaving) values('$name','$email','$phone','$address','$location','$guests','$arrival','$leaving') ";
+      mysqli_query($connection, $request);
+
+      header('location:book.php'); 
+
+   }else{
+      echo 'something went wrong please try again!';
+   }
+
+?>
